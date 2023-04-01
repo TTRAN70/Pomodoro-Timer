0c725a41574134ace77156a2ab2f0a726787dbbf
TTRAN70 <107781508+TTRAN70@users.noreply.github.com>
2023-04-01 01:14:14 -0400

    Added Countdown Timer Fuctionality

--- src/Timer.js
+++ src/Timer.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Timer.css"
 import React, { useState } from "react";
 var intervalId;
+var mode = "Session";
 
 export default function Timer() {
 
@@ -9,7 +10,6 @@ export default function Timer() {
   const [breakLength, setBreak] = useState(5);
   const [session, setSession] = useState(25);
   const [toggle, setToggle] = useState(false);
-  const [mode, setMode] = useState("Session");
   const [timerSeconds, setTimerSecond] = useState(1500);
   const [breakSeconds, setBreakSeconds] = useState(300);
 
@@ -19,14 +19,24 @@ export default function Timer() {
           setSession(prev => prev - 1);
           setTimerSecond(prev => (session-1) * 60);
          if (mode == "Session") {
-          setDisplay(prev => session-1 + ":00");
+           if (session-1 < 10) {
+            setDisplay(prev => "0".concat(session-1 + ":00"));
+           }
+           else {
+            setDisplay(prev => session-1 + ":00");
+           }
         }
        }
        else if (event.target.value == "+" && session <= 59) {
           setSession(prev => prev + 1);
           setTimerSecond(prev => (session+1) * 60);
         if (mode == "Session") {
-          setDisplay(prev => session+1 + ":00");
+          if (session+1 < 10) {
+            setDisplay(prev => "0".concat(session+1 + ":00"));
+           }
+           else {
+            setDisplay(prev => session+1 + ":00");
+           }
          }
        }
   }
@@ -38,14 +48,24 @@ export default function Timer() {
           setBreak(prev => prev - 1);
           setBreakSeconds(prev => (breakLength-1) * 60);
         if (mode == "Break") {
-          setDisplay(prev => breakLength-1 + ":00");
+          if (breakLength-1 < 10) {
+            setDisplay(prev => "0".concat(breakLength-1 + ":00"));
+           }
+           else {
+            setDisplay(prev => breakLength-1 + ":00");
+           }
         }
       }
       else if (event.target.value == "+" && breakLength <= 59) {
         setBreak(prev => prev + 1);
         setBreakSeconds(prev => (breakLength+1) * 60);
        if (mode == "Break") {
-        setDisplay(prev => breakLength+1 + ":00");
+        if (breakLength+1 < 10) {
+          setDisplay(prev => "0".concat(breakLength+1 + ":00"));
+         }
+         else {
+          setDisplay(prev => breakLength+1 + ":00");
+         }
         }
       }
    }
@@ -56,7 +76,7 @@ export default function Timer() {
     setBreak(5);
     setSession(25);
     setToggle(false);
-    setMode("Session");
+    mode = "Session";
     setTimerSecond(1500);
     setBreakSeconds(300);
     clearInterval(intervalId);
@@ -70,20 +90,52 @@ export default function Timer() {
           var seconds = parseInt(timeleft % 60, 10);
           minutes = minutes < 10 ? "0" + minutes : minutes;
           seconds = seconds < 10 ? "0" + seconds : seconds;
+          if (timeleft <= 0) {
+            mode = "Break";
+            clearInterval(intervalId);
+            pause();
+          }
           setDisplay(prev => minutes + ":" + seconds);
           timeleft -= 1;
         }, 1000);
+    }
+    else if (mode == "Break") {
+      var timeleft = breakSeconds;
+      intervalId = setInterval(() => {
+        var minutes = parseInt(timeleft / 60, 10);
+        var seconds = parseInt(timeleft % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        if (timeleft <= 0) {
+          mode = "Session";
+          clearInterval(intervalId);
+          pause();
+        }
+        setDisplay(prev => minutes + ":" + seconds);
+        timeleft -= 1;
+      }, 1000);
     }
   }
 
   const pause = () => {
     setToggle(prev => !toggle);
-    if (!toggle) {
-      timing();
+    if (mode == "Session") {
+      if (!toggle) {
+        timing();
+      }
+      else {
+        setTimerSecond(prev => (parseInt(display.substring(0,2), 10) * 60) + parseInt(display.substring(3, display.length), 10));
+        clearInterval(intervalId);
+      }
     }
-    else {
-      setTimerSecond(prev => (parseInt(display.substring(0,2), 10) * 60) + parseInt(display.substring(3, display.length), 10));
-      clearInterval(intervalId);
+    else if (mode == "Break") {
+      if (!toggle) {
+        timing();
+      }
+      else {
+        setBreakSeconds(prev => (parseInt(display.substring(0,2), 10) * 60) + parseInt(display.substring(3, display.length), 10));
+        clearInterval(intervalId);
+      }
     }
   }
 
