02db3460f37bda45c1762fe08c0eaa3056b047b3
TTRAN70 <107781508+TTRAN70@users.noreply.github.com>
2023-04-04 23:38:40 -0400

    added media queries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
 import Timer from './Timer'
+import ReactFCCtest from 'react-fcctest';
 
 export default function App() {
   return (
     <div className="mainwrapper">
+      <ReactFCCtest />
       <Timer />
     </div>
   );
